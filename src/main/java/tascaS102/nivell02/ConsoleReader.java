d4f8e1405d9add8eaa9a67dd339deb1d0062ec5c
Bernat Mestres <ber.mestres@gmail.com>
2026-06-11 12:39:59 +0200

    feature: added parse and return functionality to ConsoleReader

--- src/main/java/tascaS102/nivell02/ConsoleReader.java
+++ src/main/java/tascaS102/nivell02/ConsoleReader.java
@@ -7,20 +7,37 @@ public class ConsoleReader {
 
     private static Scanner input = new Scanner(System.in);
 
-    public static void readByte(String message){
+    public static byte readByte(String message){
         System.out.println("Enter a byte:");
-        String userValue = input.nextLine();
+        String userInput = input.nextLine();
+        byte formatedByte = Byte.parseByte(userInput);
+        return formatedByte;
     }
 
-    public static void readInt(String message){
-        System.out.println("Enter an integer value:");
+    public static int readInt(String message){
+        System.out.println("Enter an integer:");
+        String userInput = input.nextLine();
+        int formatedInt = Integer.parseInt(userInput);
+        return formatedInt;
     }
 
-    public static void readFloat(String message){
+    public static float readFloat(String message){
         System.out.println("Enter a float value:");
+        String userInput = input.nextLine();
+        float formatedFloat = Float.parseFloat(userInput);
+        return formatedFloat;
     }
 
-    public static void readDouble(String message){
+    public static double readDouble(String message){
         System.out.println("Enter a double value:");
+        String userInput = input.nextLine();
+        double formatedDouble = Double.parseDouble(userInput);
+        return formatedDouble;
+    }
+
+    public static char readChar(String message){
+        System.out.println("Enter a character");
+        char character = input.nextLine().charAt(0);
+        return character;
     }
 }
