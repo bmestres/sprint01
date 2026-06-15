1dbf53f401f690eb95449b6828cd8ff7ae5d7914
Bernat Mestres <ber.mestres@gmail.com>
2026-06-15 12:35:53 +0200

    feat: created Main class which creates an array list of months and adds a missing element at its index

---
+++ src/main/java/tascaS103/nivell01/Month.java
@@ -0,0 +1,26 @@
+package tascaS103.nivell01;
+
+public class Month {
+
+    // Attributes
+    private String name;
+
+    // Constructor
+    public Month(String name) {
+
+        this.name = name;
+    }
+    // Setters
+    public void setName(String name){
+        this.name = name;
+    }
+    // Getters
+    public String getName(){
+        return this.name;
+    }
+
+    @Override
+    public String toString() {
+        return String.format("%s", this.name);
+    }
+}
