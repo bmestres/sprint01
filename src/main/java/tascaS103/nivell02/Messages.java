dbacb8aba271b9c284c623a4a28b391d97125315
Bernat Mestres <ber.mestres@gmail.com>
2026-06-21 15:56:44 +0200

    feat: override of Comparable interfice to compare restaurants alphabeticall and by score if alphabetically equal

---
+++ src/main/java/tascaS103/nivell02/Messages.java
@@ -0,0 +1,5 @@
+package tascaS103.nivell02;
+
+public class Messages {
+    public static final String E_NULL_COMPARE = "Cannot compare with NULL";
+}
--- src/main/java/tascaS103/nivell02/Restaurant.java
+++ src/main/java/tascaS103/nivell02/Restaurant.java
@@ -2,19 +2,19 @@ package tascaS103.nivell02;
 
 import java.util.Objects;
 
-public class Restaurant {
+public class Restaurant implements Comparable<Restaurant> {
     private String name;
     private int score;
 
     @Override
     public boolean equals(Object object) {
-        if(object == this){
+        if (object == this) {
             return true;
         }
-        if(!(object instanceof Restaurant)){
+        if (!(object instanceof Restaurant)) {
             return false;
         }
-        Restaurant restaurant = (Restaurant)object;
+        Restaurant restaurant = (Restaurant) object;
         return this.name.equalsIgnoreCase(restaurant.name) && this.score == restaurant.score;
     }
 
@@ -22,6 +22,21 @@ public class Restaurant {
     public int hashCode() {
         return Objects.hash(name, score);
     }
+
+    @Override
+    public int compareTo(Restaurant other) {
+        if (other == null) {
+            throw new NullPointerException(Messages.E_NULL_COMPARE);
+        }
+        int compare = this.name.compareToIgnoreCase(other.name);
+
+        if(compare == 0){
+            return Integer.compare(other.score, this.score);
+        }
+        return compare;
+    }
 }
+
+
 
 
